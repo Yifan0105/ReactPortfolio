341db53d7785c1d7130b7dd53d851dd4c4a191e3
Yifan SHI <yifanshi0105@gmail.com>
2024-12-09 18:55:22 +0100

    update i18n for Formation

--- src/componets/Formations/Formations.jsx
+++ src/componets/Formations/Formations.jsx
@@ -1,57 +1,59 @@
 import React from 'react';
 import styles from './Formations.module.css';
+import { translate } from '../../i18n/i18n';
 
-export const Formations = () => {
+export const Formations = ({ lang }) => {
   return (
     <section id="formations" className={styles.container}>
-      <h2 className={styles.title}> Mes Formations  </h2>
+      <h2 className={styles.title}> {translate(lang, 'formations.title')} </h2>
       <div className={styles.content}>
         <img src="./assets/formations/formation.JPG" alt="2024 Graduation" className={styles.formationImage}/>
         <ul className={styles.formationItems}>
           <li className={styles.formationItem}>
             <img src="./assets/formations/UT1.png" alt="UT1 icon" className={styles.formationUniversityImage}/>
               <div className={styles.formationItemText}>
-                <h3>Master MIAGE (Méthodes Informatiques Appliquées à la Gestion des Entreprises) </h3>
-                Faculté d'informatique | Université de Toulouse 1 Capitole (UT1) 
+                <h3>{translate(lang, 'formations.master.title')}</h3>
+                {translate(lang, 'formations.master.university')}
                 <br />
-                Toulouse, France | 09/2022 – 10/2024 
+                {translate(lang, 'formations.master.location')} | {translate(lang, 'formations.master.dates')}
                 <br /><br />
-                <p>M2 MIAGE IPM (Ingénierie des Processus Métier)</p>
-                <p> M1 MIAGE IM (Ingénierie Métier)</p>
-
+                <p className={styles.keywordText}>{translate(lang, 'formations.master.specialization1')}</p>
+                {translate(lang, 'formations.master.courses')}
+                <p className={styles.keywordText}>{translate(lang, 'formations.master.specialization2')}</p>
+                {translate(lang, 'formations.master.courses2')}
               </div>
           </li>
           <li className={styles.formationItem}>
-            <img src="./assets/formations/UT1.png" alt="UT1 icon" className={styles.formationUniversityImage} /> 
+            <img src="./assets/formations/UT1.png" alt="UT1 icon" className={styles.formationUniversityImage}/> 
               <div className={styles.formationItemText}>
-                <h3>Année préparatoire chinois pour Master MIAGE</h3>
-                 Faculté d'informatique | Université de Toulouse 1 Capitole (UT1) 
-                 <br />
-                 Toulouse, France | 09/2021 - 06/2022
+                <h3>{translate(lang, 'formations.preparatory.title')}</h3>
+                {translate(lang, 'formations.preparatory.university')}
+                <br />
+                {translate(lang, 'formations.preparatory.location')} | {translate(lang, 'formations.preparatory.dates')}
               </div>
           </li>
           <li className={styles.formationItem}>
             <img src="./assets/formations/CUEB.png" alt="CUEB icon" className={styles.formationUniversityImage}/>
               <div className={styles.formationItemText}>
-                <h3>Licence en Gestion des Ressources Humaines </h3>
-                School of Labor Economics | Capital University of Economics and Business (CUEB)
+                <h3>{translate(lang, 'formations.bachelor.title')}</h3>
+                {translate(lang, 'formations.bachelor.university')}
                 <br />
-                Pékin, Chine | 09/2017 - 07/2021 
+                {translate(lang, 'formations.bachelor.location')} | {translate(lang, 'formations.bachelor.dates')}
               </div>
           </li>
           <li className={styles.formationItem}>
             <img src="./assets/formations/DCU.png" alt="DCU icon" className={styles.formationUniversityImage}/>
               <div className={styles.formationItemText}>
-                <h3>Étudiante en échange à Dublin  </h3>
-                Business school | Dublin City University (DCU) 
+                <h3>{translate(lang, 'formations.exchange.title')}</h3>
+                {translate(lang, 'formations.exchange.university')}
                 <br />
-                Dublin, Irlande | 08/2019 - 01/2020 
-                <p>
-                </p>
+                {translate(lang, 'formations.exchange.location')} | {translate(lang, 'formations.exchange.dates')}
+                <p></p>
               </div>
           </li>
         </ul>
       </div>
     </section>
-  )
+  );
 }
+
